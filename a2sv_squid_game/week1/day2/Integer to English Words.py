c50f80140ae2cb90e8b40629bc8201ce85a596e6
lealemgetahun <lealemgetahun73@gmail.com>
2023-02-02 21:32:19 +0300

    a2sv squid game day 2 4-

--- a2sv_squid_game/week1/day2/Integer to English Words.py
+++ a2sv_squid_game/week1/day2/Integer to English Words.py
@@ -19,9 +19,9 @@ class Solution:
     def numberToWords(self, num):
 
         dic = {
-            "0-20": ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"],
-            "tens": ["","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety","Hundred"],
-            "thousands": ["","Thousand","Million","Billion"]
+            "0-20": {0:"",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Eleven",12:"Twelve",13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Nineteen"},
+            "tens": {0:"",1:"Ten",2:"Twenty",3:"Thirty",4:"Forty",5:"Fifty",6:"Sixty",7:"Seventy",8:"Eighty",9:"Ninety",10:"Hundred"},
+            "thousands": {0:"",1:"Thousand",2:"Million",3:"Billion"}
         }
 
         if num == 0:
